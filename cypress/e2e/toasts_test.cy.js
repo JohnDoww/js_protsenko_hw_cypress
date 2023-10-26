0a602001a253467c3b06969600078f032d4f7dfc
Andrey <andreyprotsenko99@gmail.com>
2023-10-26 22:55:31 +0300

    added toasts

---
+++ cypress/e2e/toasts_test.cy.js
@@ -0,0 +1,48 @@
+
+describe('tost test', () => {
+    it('passes', () => {
+        cy.visit('https://sanitarskyi-ngx-admin.herokuapp.com/');
+        cy.get('[alt="Dark Theme"]').click();
+        cy.get('.menu-title.ng-tns-c141-19').click();
+        cy.get('[title="Toastr"]').click();
+
+
+        cy.get('.form-group .select-button').eq(0).click();
+        cy.get('#nb-option-30').click();
+        cy.get('[ng-reflect-name="title"]')
+            .clear()
+            .type('Test Testenko')
+            .should('have.value', 'Test Testenko');
+        cy.get('[ng-reflect-name="content"]')
+            .clear()
+            .type('Awesome content is here')
+            .should('have.value', 'Awesome content is here');
+        cy.get('.form-group .select-button').eq(1).click();
+        cy.get('#nb-option-35').click();
+
+        cy.get('nb-card-footer .mat-ripple').eq(0).click();
+        cy.get('nb-toastr-container .ng-tns-c209-54.ng-star-inserted').should("be.visible");
+        cy.get('nb-toastr-container .ng-tns-c209-54.ng-star-inserted').then( elementWhichIFound => {
+            expect(elementWhichIFound).to.contain.text('Test Testenko');
+            expect(elementWhichIFound).to.contain.text('Awesome content is here');
+            // expect(tableCell).to.have.html('Column content');
+            // expect(tableCell).to.contain('Column content');
+            // expect(tableCell).to.contain(' content');
+            // expect(tableCell.text()).to.include('Column content');
+            // expect(tableCell.text()).to.include(' content');
+            // expect(tableCell).to.not.contain('qwe12');
+        })
+
+
+        /**
+         * +++++3. Клікнути на пункт меню Modal & Overlays
+         * +++4. Клікнути на підпункт Toastr
+         * 5. Вибрати позицію тосту+ 4 types / top right top left, bottom right bottom left
+         * 6. Заповнити title довільним текстом+
+         * 7. заповнити content довільним текстом+
+         * 8. Обрати тип тоста+/
+         * 4 types - check background and picture
+         * 9. Клікнути на кнопку "Show toast"
+         */
+    })
+})
